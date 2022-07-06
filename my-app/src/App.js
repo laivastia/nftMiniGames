a047448a443604fb1d6b4258b83d6110382602e2
Vastia <zmfwl123@gmail.com>
2022-07-06 15:05:56 +0900

    edit project setting
    move for page component
    create 404 error page component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Faq from "./pages/faq";
-import Gallery from "./pages/Gallery";
-import Main from "./pages/Main";
 import Nav from "./pages/Nav";
-import RoadMap from "./pages/roadMap";
+import Main from "./pages/Main";
+import Gallery from "./pages/Gallery";
 import Story from "./pages/Story";
-import Tos from "./pages/Tos";
+import RoadMap from "./pages/roadMap";
 import Utility from "./pages/utility";
+import Faq from "./pages/Faq";
+import Tos from "./pages/Tos";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/story" element={<Story />} />
-          <Route path="/roadmap" element={<RoadMap />} />
-          <Route path="/utility" element={<Utility />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/tos" element={<Tos />} />
-        </Routes>
-      </Nav>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/story" element={<Story />} />
+        <Route path="/roadmap" element={<RoadMap />} />
+        <Route path="/utility" element={<Utility />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/tos" element={<Tos />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ my-app/src/pages/Error.js
@@ -0,0 +1,5 @@
+function Error() {
+  return <div className="error"></div>;
+}
+
+export default Error;
